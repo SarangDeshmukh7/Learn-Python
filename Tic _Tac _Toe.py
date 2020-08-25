da51a3c46e3dd6508a3147b83fe412bb5c9f7527
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-08-25 19:24:34 +0530

    Update Tic _Tac _Toe.py

--- Tic _Tac _Toe.py
+++ Tic _Tac _Toe.py
@@ -1,3 +1,7 @@
+'''
+Tic-tac-toe (American English), noughts and crosses (Commonwealth English), or Xs and Os is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
+'''
+
 import random
 
 
