57ae4ea55d368f6ef53b677ae21afce85833aa0b
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-08-27 14:44:36 +0530

    Update Draw_game_board.py

--- Draw_game_board.py
+++ Draw_game_board.py
@@ -1,6 +1,4 @@
 '''
-This exercise is Part 1 of 4 of the Tic Tac Toe exercise series.
-The other exercises are: Part 2, Part 3, and Part 4.
 Time for some fake graphics! Let’s say we want to draw game boards
 that look like this:
  --- --- --- 
